7c2539780084c0ed36bff619a86cf101da02bc3b
Azemoytin <andrewzemoytin2005@gmail.com>
2025-07-13 20:20:26 -0700

    added addTask(), completTask(), reportMood()

--- assistant.js
+++ assistant.js
@@ -4,3 +4,22 @@ function PersonalAssistant(name) {
   this.tasks = [];
   this.mood = "neutral";
 }
+
+  this.addTask = function(task) {
+    this.tasks.push(task);
+    console.log(`${this.name} added a new task: "${task}"`);
+  };
+
+  this.completeTask = function() {
+    if (this.tasks.length > 0) {
+      const completed = this.tasks.shift();
+      console.log(`${this.name} completed the task: "${completed}"`);
+    } else {
+      console.log(`${this.name} has no tasks to complete.`);
+    }
+  };
+
+  this.reportMood = function() {
+    console.log(`${this.name}'s current mood is: ${this.mood}`);
+  };
+}
